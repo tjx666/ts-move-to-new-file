ff516c7089883376487ee8034dfbaf1334a8935d
余腾靖 <ytj2713151713@gmail.com>
2024-08-15 19:08:34 +0800

    init

---
+++ components/main.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export const main = () => {};
+
+interface SidebarSectionProps {
+    icon: string;
+    title: string;
+}
+
+function SidebarSection({ children }: SidebarSectionProps) {
+    return <div>123</div>;
+}
